b9773b197b2fac78459e8bda23aea02b8d9b91ef
taax6428 <jana.weber@uni-oldenburg.de>
2025-04-28 20:30:12 +0200

    Added Migrations

---
+++ Migrations/20250428110155_addedAttributesToAlgorithm.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaWebtool2.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedAttributesToAlgorithm : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Algorithms",
+                newName: "ModelUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ModelUrl",
+                table: "Algorithms",
+                newName: "ImageUrl");
+        }
+    }
+}
